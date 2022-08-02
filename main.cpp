b7e7da91a885fbf498548c5e770f5efc39ee1e67
Suleymanov Andrey <sasus37@gmail.com>
2022-08-02 19:00:30 +0300

    DONE

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+map<char,int> WordComposition (string &str){
+    map<char,int> tmp;
+    for (int i = 0; i < str.length(); i++){
+        tmp[(char)tolower(str[i])]++;
+    }
+    return tmp;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << "Enter 2 words: ";
+    string word1, word2;
+    cin >> word1 >> word2;
+
+    if (WordComposition(word1) == WordComposition (word2)){
+        cout << "These words are an anagram";
+    }else {cout << "These words are not an anagram";}
 }
